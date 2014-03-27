bfc0d9413ed1a8907b49e5d607eed932a8272df1
RK <robert@woodst.com>
2014-03-27 11:55:11 -0400

    Fixing private method definition, used === instead of ==.
    When implementing private methods, the last thing you want to do in a
    constructor is to return an object other than "this". Depending on the
    environment this could lead to leaks and undefined behavior. Instead, all
    priviledged methods (which can access private info) should be defined on "this".
    
    Otherwise, intended use would be for the SV.Player method to be used as a method
    not a constructor:
    
        var player = SV.Player('my-video-id');
        // Instead of
        var player = new SV.Player('my-video-id');

--- src/player_api.js
+++ src/player_api.js
@@ -9,9 +9,15 @@ if (!SV) {
 
     if (!SV.Player) {
         SV.Player = function(options) {
-            var _videoId = options.videoId;
-            var _playlistId = options.playlistId;
-            var _volume = 1,_duration = 0,_currentTime = 0,_loaded = 0,_email = null,_playing = false,_listeners={};
+            var _videoId = options.videoId,
+                _playlistId = options.playlistId,
+                _volume = 1,
+                _duration = 0,
+                _currentTime = 0,
+                _loaded = 0,
+                _email = null,
+                _playing = false,
+                _listeners = {};
 
             var _sendMessage = function(message) {
                 _iframe.contentWindow.postMessage(message, window.location.protocol + '//videos.sproutvideo.com');
@@ -33,124 +39,120 @@ if (!SV) {
                 throw 'Can not find video iframe';
             }
 
-            var public = {
-                events: options.events,
+            this.events = options.events;
 
-                play: function(index) {
-                    if (typeof index != 'undefined') {
-                        _sendMessage('{"name":"playVideo", "data":"' + index + '"}');
-                    } else {
-                        _sendMessage('{"name":"play"}');
-                    }
-                },
+            this.play = function(index) {
+                if (typeof index !== 'undefined') {
+                    _sendMessage('{"name":"playVideo", "data":"' + index + '"}');
+                } else {
+                    _sendMessage('{"name":"play"}');
+                }
+            };
 
-                pause: function() {
-                    _sendMessage('{"name":"pause"}');
-                },
+            this.pause = function() {
+                _sendMessage('{"name":"pause"}');
+            };
 
-                setVolume: function(vol) {
-                    _sendMessage('{"name":"volume", "data":"' + vol + '"}');
-                },
+            this.setVolume = function(vol) {
+                _sendMessage('{"name":"volume", "data":"' + vol + '"}');
+            };
 
-                getVolume: function() {
-                    return _volume;
-                },
+            this.getVolume = function() {
+                return _volume;
+            };
 
-                seek: function(loc) {
-                    _sendMessage('{"name":"seek", "data":"' + loc + '"}');
-                },
+            this.seek = function(loc) {
+                _sendMessage('{"name":"seek", "data":"' + loc + '"}');
+            };
 
-                toggleHD: function() {
-                    _sendMessage('{"name":"toggleHD"}');
-                },
+            this.toggleHD = function() {
+                _sendMessage('{"name":"toggleHD"}');
+            };
 
-                getCurrentTime: function() {
-                  return _currentTime;
-                },
+            this.getCurrentTime = function() {
+                return _currentTime;
+            };
 
-                getPercentLoaded: function() {
-                  return _loaded;
-                },
+            this.getPercentLoaded = function() {
+                return _loaded;
+            };
 
-                getDuration: function() {
-                  return _duration;
-                },
+            this.getDuration = function() {
+                return _duration;
+            };
 
-                getEmail: function() {
-                    return _email;
-                },
+            this.getEmail = function() {
+                return _email;
+            };
 
-                getPlaying: function () {
-                    return _playing;
-                },
+            this.getPlaying = function() {
+                return _playing;
+            };
 
-                updateStatus: function(message) {
-                    switch(message.type){
-                        case 'volume':
-                            _volume = message.data;
-                            break;
-                        case 'progress':
-                            _currentTime = message.data.time;
-                            break;
-                        case 'loading':
-                            _loaded = message.data;
-                            break;
-                        case 'ready':
-                            _duration = message.data.duration;
-                            _email = message.data.email;
-                            break;
-                        case 'play':
-                        case 'playVideo':
-                            _playing = true;
-                            break;
-                        case 'pause':
-                            _playing = false;
-                            break;
-                    }
-                },
+            this.updateStatus = function(message) {
+                switch(message.type) {
+                    case 'volume':
+                        _volume = message.data;
+                        break;
+                    case 'progress':
+                        _currentTime = message.data.time;
+                        break;
+                    case 'loading':
+                        _loaded = message.data;
+                        break;
+                    case 'ready':
+                        _duration = message.data.duration;
+                        _email = message.data.email;
+                        break;
+                    case 'play':
+                    case 'playVideo':
+                        _playing = true;
+                        break;
+                    case 'pause':
+                    case 'completed':
+                        _playing = false;
+                        break;
+                }
+            };
 
-                bind: function(type, listener) {
-                    if(typeof _listeners[type] == 'undefined') {
-                        _listeners[type] = [];
-                    }
-                    _listeners[type].push(listener);
-                },
+            this.bind = function(type, listener) {
+                if (typeof _listeners[type] === 'undefined') {
+                    _listeners[type] = [];
+                }
+                _listeners[type].push(listener);
+            };
 
-                fire: function(event) {
-                    if (typeof event == 'string') {
-                        event = {type: event};
-                    }
-                    if (!event.target) {
-                        event.target = this;
-                    }
-                    if (_listeners[event.type] instanceof Array){
-                        var listeners = _listeners[event.type];
-                        for (var i=0, len=listeners.length; i < len; i++){
-                            var returnValue = listeners[i].call(this, event);
-                            if(returnValue == this.unbind) {
-                                this.unbind(event.type, listeners[i]);
-                            }
-                        }
-                    }
-                },
-
-                unbind: function(type, listener) {
-                    if (_listeners[type] instanceof Array){
-                        var listeners = _listeners[type];
-                        for (var i=0, len=listeners.length; i < len; i++){
-                            if (listeners[i] === listener){
-                                listeners.splice(i, 1);
-                                break;
-                            }
+            this.fire = function(event) {
+                if (typeof event === 'string') {
+                    event = {type: event};
+                }
+                if (!event.target) {
+                    event.target = this;
+                }
+                if (_listeners[event.type] instanceof Array){
+                    var listeners = _listeners[event.type];
+                    for (var i=0, len=listeners.length; i < len; i++) {
+                        var returnValue = listeners[i].call(this, event);
+                        if (returnValue === this.unbind) {
+                            this.unbind(event.type, listeners[i]);
                         }
                     }
                 }
-
             };
 
-            SV.players[_videoId||_playlistId] = public;
+            this.unbind = function(type, listener) {
+                if (_listeners[type] instanceof Array){
+                    var listeners = _listeners[type];
+                    for (var i=0, len=listeners.length; i < len; i++) {
+                        if (listeners[i] === listener) {
+                            listeners.splice(i, 1);
+                            break;
+                        }
+                    }
+                }
+            };
 
-            return public;
+            SV.players[_videoId||_playlistId] = this;
         };
 
     }
@@ -161,7 +163,7 @@ if (!SV) {
                 if (document.getElementsByClassName) {
                     return document.getElementsByClassName(classname);
                 } else {
-                    var classElements = new Array();
+                    var classElements = [];
                     var els = document.getElementsByTagName('*');
                     var elsLen = els.length;
                     var pattern = new RegExp("(^|\\s)" + classname + "(\\s|$)");
